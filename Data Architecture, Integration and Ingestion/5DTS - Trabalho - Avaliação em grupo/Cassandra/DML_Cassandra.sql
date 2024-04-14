7502245f64999062cf62ddfe2d8c1aca2add7b30
Fabio Souza <fabmasouza@hotmail.com>
2024-04-14 15:40:08 -0300

    commitando trabalhos desenvolvidos

---
+++ Data Architecture, Integration and Ingestion/5DTS - Trabalho - Avaliação em grupo/Cassandra/DML_Cassandra.sql
@@ -0,0 +1,164 @@
+--SELECIONANDO O BANCO
+USE avaliacao_grupo;
+
+--INSERINDO DADOS NA TABLE Produto
+INSERT INTO Produto (ID_Produto, Nome, Modelo, Fabricante, Valor, Cor, Tamanho) VALUES (1, 'PRODUTO 1', 'MODELO 1', 'FABRICANTE 1', 1, 
+{'Cor de Banana com Tomate',
+'Cor de A Vingadora',
+'Cor de Caramelo com Borbulhas',
+'Cor de Burro Quando Foge',
+'Cor de Estomago com Fome',
+'Cor de Batata',
+'Cor de Garrafa de Cafe com Nausea'},
+['P',
+'M',
+'G',
+'38',
+'39',
+'40',
+'41']);
+INSERT INTO Produto (ID_Produto, Nome, Modelo, Fabricante, Valor, Cor, Tamanho) VALUES (2, 'PRODUTO 2', 'MODELO 2', 'FABRICANTE 2', 2, 
+{'Cor de Banana com Tomate',
+'Cor de A Vingadora',
+'Cor de Caramelo com Borbulhas',
+'Cor de Burro Quando Foge',
+'Cor de Estomago com Fome',
+'Cor de Batata',
+'Cor de Garrafa de Cafe com Nausea'},
+['P',
+'M',
+'G',
+'38',
+'39',
+'40',
+'41']);
+INSERT INTO Produto (ID_Produto, Nome, Modelo, Fabricante, Valor, Cor, Tamanho) VALUES (3, 'PRODUTO 3', 'MODELO 3', 'FABRICANTE 3', 3, 
+{'Cor de Banana com Tomate',
+'Cor de A Vingadora',
+'Cor de Caramelo com Borbulhas',
+'Cor de Burro Quando Foge',
+'Cor de Estomago com Fome',
+'Cor de Batata',
+'Cor de Garrafa de Cafe com Nausea'},
+['P',
+'M',
+'G',
+'38',
+'39',
+'40',
+'41']);
+INSERT INTO Produto (ID_Produto, Nome, Modelo, Fabricante, Valor, Cor, Tamanho) VALUES (4, 'PRODUTO 4', 'MODELO 4', 'FABRICANTE 4', 4, 
+{'Cor de Banana com Tomate',
+'Cor de A Vingadora',
+'Cor de Caramelo com Borbulhas',
+'Cor de Burro Quando Foge',
+'Cor de Estomago com Fome',
+'Cor de Batata',
+'Cor de Garrafa de Cafe com Nausea'},
+['P',
+'M',
+'G',
+'38',
+'39',
+'40',
+'41']);
+INSERT INTO Produto (ID_Produto, Nome, Modelo, Fabricante, Valor, Cor, Tamanho) VALUES (5, 'PRODUTO 5', 'MODELO 5', 'FABRICANTE 5', 5, 
+{'Cor de Banana com Tomate',
+'Cor de A Vingadora',
+'Cor de Caramelo com Borbulhas',
+'Cor de Burro Quando Foge',
+'Cor de Estomago com Fome',
+'Cor de Batata',
+'Cor de Garrafa de Cafe com Nausea'});
+INSERT INTO Produto (ID_Produto, Nome, Modelo, Fabricante, Valor, Cor, Tamanho) VALUES (6, 'PRODUTO 6', 'MODELO 6', 'FABRICANTE 6', 6, 
+{'Cor de Banana com Tomate',
+'Cor de A Vingadora',
+'Cor de Caramelo com Borbulhas',
+'Cor de Burro Quando Foge',
+'Cor de Estomago com Fome',
+'Cor de Batata',
+'Cor de Garrafa de Cafe com Nausea'},
+['P',
+'M',
+'G',
+'38',
+'39',
+'40',
+'41']);
+INSERT INTO Produto (ID_Produto, Nome, Modelo, Fabricante, Valor, Cor, Tamanho) VALUES (7, 'PRODUTO 7', 'MODELO 7', 'FABRICANTE 7', 7, 
+{'Cor de Banana com Tomate',
+'Cor de A Vingadora',
+'Cor de Caramelo com Borbulhas',
+'Cor de Burro Quando Foge',
+'Cor de Estomago com Fome',
+'Cor de Batata',
+'Cor de Garrafa de Cafe com Nausea'},
+['P',
+'M',
+'G',
+'38',
+'39',
+'40',
+'41']);
+INSERT INTO Produto (ID_Produto, Nome, Modelo, Fabricante, Valor, Cor, Tamanho) VALUES (8, 'PRODUTO 8', 'MODELO 8', 'FABRICANTE 8', 8, 
+{'Cor de Banana com Tomate',
+'Cor de A Vingadora',
+'Cor de Caramelo com Borbulhas',
+'Cor de Burro Quando Foge',
+'Cor de Estomago com Fome',
+'Cor de Batata',
+'Cor de Garrafa de Cafe com Nausea'},
+['P',
+'M',
+'G',
+'38',
+'39',
+'40',
+'41']);
+INSERT INTO Produto (ID_Produto, Nome, Modelo, Fabricante, Valor, Cor, Tamanho) VALUES (9, 'PRODUTO 9', 'MODELO 9', 'FABRICANTE 9', 9, 
+{'Cor de Banana com Tomate',
+'Cor de A Vingadora',
+'Cor de Caramelo com Borbulhas',
+'Cor de Burro Quando Foge',
+'Cor de Estomago com Fome',
+'Cor de Batata',
+'Cor de Garrafa de Cafe com Nausea'},
+['P',
+'M',
+'G',
+'38',
+'39',
+'40',
+'41']);
+
+--INSERINDO DADOS NA TABLE Cliente
+INSERT INTO Cliente (ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone) VALUES (1, '01234567891', 'CLIENTE 1', 'ENDERECO 1', '01234-123', 'EMAIL1@GMAIL.COM', '(43) 12345-1234');
+INSERT INTO Cliente (ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone) VALUES (2, '01234567891', 'CLIENTE 2', 'ENDERECO 2', '01234-123', 'EMAIL2@GMAIL.COM', '(43) 12345-1234');
+INSERT INTO Cliente (ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone) VALUES (3, '01234567891', 'CLIENTE 3', 'ENDERECO 3', '01234-123', 'EMAIL3@GMAIL.COM', '(43) 12345-1234');
+INSERT INTO Cliente (ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone) VALUES (4, '01234567891', 'CLIENTE 4', 'ENDERECO 4', '01234-123', 'EMAIL4@GMAIL.COM', '(43) 12345-1234');
+INSERT INTO Cliente (ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone) VALUES (5, '01234567891', 'CLIENTE 5', 'ENDERECO 5', '01234-123', 'EMAIL5@GMAIL.COM', '(43) 12345-1234');
+INSERT INTO Cliente (ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone) VALUES (6, '01234567891', 'CLIENTE 6', 'ENDERECO 6', '01234-123', 'EMAIL6@GMAIL.COM', '(43) 12345-1234');
+INSERT INTO Cliente (ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone) VALUES (7, '01234567891', 'CLIENTE 7', 'ENDERECO 7', '01234-123', 'EMAIL7@GMAIL.COM', '(43) 12345-1234');
+INSERT INTO Cliente (ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone) VALUES (8, '01234567891', 'CLIENTE 8', 'ENDERECO 8', '01234-123', 'EMAIL8@GMAIL.COM', '(43) 12345-1234');
+INSERT INTO Cliente (ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone) VALUES (9, '01234567891', 'CLIENTE 9', 'ENDERECO 9', '01234-123', 'EMAIL9@GMAIL.COM', '(43) 12345-1234');
+INSERT INTO Cliente (ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone) VALUES (10, '01234567891', 'CLIENTE 10', 'ENDERECO 10', '01234-123', 'EMAIL10@GMAIL.COM', '(43) 12345-1234');
+
+--INSERINDO DADOS NA TABLE Pedido
+INSERT INTO Pedido (ID_Pedido, ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone, Produtos) VALUES (1, 1, '01234567891', 'CLIENTE 1', 'ENDERECO 1', '01234-123', 'EMAIL1@GMAIL.COM', '(43) 12345-1234',
+(9, 'PRODUTO 9', 'MODELO 9', 'FABRICANTE 9', 9, 'Cor de Banana com Tomate', 'P'));                                                                                         
+INSERT INTO Pedido (ID_Pedido, ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone, Produtos) VALUES (2, 2, '01234567891', 'CLIENTE 2', 'ENDERECO 2', '01234-123', 'EMAIL2@GMAIL.COM', '(43) 12345-1234',
+(9, 'PRODUTO 9', 'MODELO 9', 'FABRICANTE 9', 9, 'Cor de Banana com Tomate', 'P'));                                                                                         
+INSERT INTO Pedido (ID_Pedido, ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone, Produtos) VALUES (3, 3, '01234567891', 'CLIENTE 3', 'ENDERECO 3', '01234-123', 'EMAIL3@GMAIL.COM', '(43) 12345-1234',
+(9, 'PRODUTO 9', 'MODELO 9', 'FABRICANTE 9', 9, 'Cor de Banana com Tomate', 'P'));                                                                                         
+INSERT INTO Pedido (ID_Pedido, ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone, Produtos) VALUES (4, 4, '01234567891', 'CLIENTE 4', 'ENDERECO 4', '01234-123', 'EMAIL4@GMAIL.COM', '(43) 12345-1234',
+(9, 'PRODUTO 9', 'MODELO 9', 'FABRICANTE 9', 9, 'Cor de Banana com Tomate', 'P'));                                                                                         
+INSERT INTO Pedido (ID_Pedido, ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone, Produtos) VALUES (5, 5, '01234567891', 'CLIENTE 5', 'ENDERECO 5', '01234-123', 'EMAIL5@GMAIL.COM', '(43) 12345-1234',
+(9, 'PRODUTO 9', 'MODELO 9', 'FABRICANTE 9', 9, 'Cor de Banana com Tomate', 'P'));                                                                                         
+INSERT INTO Pedido (ID_Pedido, ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone, Produtos) VALUES (6, 6, '01234567891', 'CLIENTE 6', 'ENDERECO 6', '01234-123', 'EMAIL6@GMAIL.COM', '(43) 12345-1234',
+(9, 'PRODUTO 9', 'MODELO 9', 'FABRICANTE 9', 9, 'Cor de Banana com Tomate', 'P'));                                                                                         
+INSERT INTO Pedido (ID_Pedido, ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone, Produtos) VALUES (7, 7, '01234567891', 'CLIENTE 7', 'ENDERECO 7', '01234-123', 'EMAIL7@GMAIL.COM', '(43) 12345-1234',
+(9, 'PRODUTO 9', 'MODELO 9', 'FABRICANTE 9', 9, 'Cor de Banana com Tomate', 'P'));                                                                                         
+INSERT INTO Pedido (ID_Pedido, ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone, Produtos) VALUES (8, 8, '01234567891', 'CLIENTE 8', 'ENDERECO 8', '01234-123', 'EMAIL8@GMAIL.COM', '(43) 12345-1234',
+(9, 'PRODUTO 9', 'MODELO 9', 'FABRICANTE 9', 9, 'Cor de Banana com Tomate', 'P'));                                                                                         
+INSERT INTO Pedido (ID_Pedido, ID_Cliente, CPF, Nome, Endereco, CEP, Email, Telefone, Produtos) VALUES (9, 9, '01234567891', 'CLIENTE 9', 'ENDERECO 9', '01234-123', 'EMAIL9@GMAIL.COM', '(43) 12345-1234',
+(9, 'PRODUTO 9', 'MODELO 9', 'FABRICANTE 9', 9, 'Cor de Banana com Tomate', 'P'));
--- Data Architecture, Integration and Ingestion/README.md
+++ Data Architecture, Integration and Ingestion/README.md
@@ -1,27 +1,57 @@
-### Disciplina
+# Disciplina
 Data Architecture, Integration and Ingestion
 
-### Professor
+## Professor
 [Ivan Ferrreira Gancev](https://www.linkedin.com/in/ivan-gancev-8571b017/)
 
-### Conteúdo
+## Conteúdo da Disciplina
 1. Pilares de arquitetura: persistência, integração e consumo
 2. Estratégias de arquitetura
 3. Tipos de tratamentos e arquiteturas
 4. Exemplos de Bancos, diferenças e usos:
-    - Bancos Relacionais
-    - Bancos Colunares
+   1. Bancos Relacionais
+   2. Bancos Colunares
 5. Exemplos de Bancos, diferenças e usos:
-    - Bancos de documentos
-    - Bancos chave-valor
-    - Bancos de Grafos
+   1. Bancos de documentos
+   2. Bancos chave-valor
+   3. Bancos de Grafos
 6. Ingestão de dados, tratamentos e manipulações
-8. Pipeline de dados, governança e qualidade
-9. Integração de dados
-    - Cargas batch, ETL, vantagens e desvantagens
-10. Eventos, APIs, NRT e casos de uso
-11. Arquiteturas para analytics
-12. Boas práticas, recomendações e cuidados
+7. Pipeline de dados, governança e qualidade
+8. Integração de dados
+   1. Cargas batch, ETL, vantagens e desvantagens
+9.  Eventos, APIs, NRT e casos de uso
+10. Arquiteturas para analytics
+11. Boas práticas, recomendações e cuidados
 
+# Trabalhos Desenvolvidos
+## Trabalho em grupo
+### Descrição:
+Trabalho em grupo (3 ou 4 integrantes)
+Parte 1:
+- Crie 3 tabelas para um site de venda de sapatos;
+- Adicionar campos, se desejar;
+- Alguns campos são conceituais, exercitem a modelagem;
+- Entrega: Os scripts de criação das tabelas para MySQL, Cassandra e MongoDB;
 
+Parte 2:
+- Insira 10 registros distintos em cada uma das tabelas;
+- Entrega: Os scripts de inserção dos registros para MySQL, Cassandra e MongoDB
 
+Parte 3:
+- Seu site comprou um concorrente. Ele te enviou 2 arquivos CSV com 20 produtos e 20 clientes;
+- Entrega: Crie os arquivos CSV com a modelagem definida na parte 1 e os scripts de carga para 1 dos 3 bancos usados no exercício;
+
+### Entregáveis
+1. Cassandra
+   1. DDL_Cassandra.sql - Criação do keyspace "avaliacao_grupo" com uma estratégia de replicação simples e definição das tabelas "Produto", "Cliente" e "Pedido"
+   2. DML_Cassandra.sql - Inserção de dados nas tabelas "Produto", "Cliente" e "Pedido" dentro do keyspace "avaliacao_grupo".
+2. MongoDB
+   1. DDL_Mongo.sql - Criação de três coleções no banco de dados "avaliacao_grupo" do MongoDB: "Produto", "Cliente" e "Pedido".
+   2. DML_Mongo.sql - Inserção dos dados nas coleções.
+   3. Importando Collections.txt - Importação dos dados em formato JSON para as coleções criadas.
+   4. produtos.json - Dados dos produtos
+   5. clientes.json - Dados dos clientes
+3. MySQL
+   1. DDL_MySQL.sql - Criação do banco de dados "avaliacao_grupo" e definição das tabelas "Produto", "Cliente" e "Pedido"
+   2. DML_MySQL.sql - Inserção de dados nas tabelas "Produto", "Cliente" e "Pedido" dentro do banco de dados "avaliacao_grupo".
+   3. Modelagem.xlsx - Modelagem do banco de dados "avaliacao_grupo".
